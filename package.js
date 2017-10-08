263d68c44fc1ef4540aa88040517783e966a14e9
Scott Donald <scottdonald@protonmail.ch>
2017-10-08 19:36:48 +1100

    Updates

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'abernix:standard-minifier-js',
+  name: 'scottdonaldau:standard-minifier-js',
   version: '2.1.0',
   summary: 'Harmony javascript minifiers used with Meteor apps by default.',
   documentation: 'README.md',
@@ -8,7 +8,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "minifyStdJS",
   use: [
-    'abernix:minifier-js@2.1.0',
+    'scottdonaldau:minifier-js@2.1.0',
     'babel-compiler@6.19.2',
     'ecmascript@0.8.0'
   ],
